1e8f5e0b48c694850c7e652fbf553a25a036b0ab
onderhamamcioglu <onder1hamamcioglu@gmail.com>
2024-11-28 16:28:41 +0100

    modulo optimization added

--- main.cpp
+++ main.cpp
@@ -28,16 +28,46 @@ void sort_data_parallelized(int* data) {
     }
 }
 
-void sort_data_mod(int* data) { //TODO Optimize the sorting algorithm further by using modulo %
-#pragma omp parallel for schedule(dynamic)
+void sort_data_mod(int* data) { // Separate odd and even numbers by using modulo
+    int oddNumbers[DATA_SIZE], evenNumbers[DATA_SIZE];
+    int oddCount = 0, evenCount = 0;
+
     for (int i = 0; i < DATA_SIZE; i++) {
-        for (int j = 0; j < DATA_SIZE - 1; j++) {
-            if (data[j] > data[j + 1]) {
-                int help = data[j];
-                data[j] = data[j + 1];
-                data[j + 1] = help;
+        if (data[i] % 2 == 0) {
+            evenNumbers[evenCount] = data[i];
+            evenCount++;
+        } else {
+            oddNumbers[oddCount] = data[i];
+            oddCount++;
+        }
+    }
+
+    for (int i = 0; i < oddCount; i++) {
+        for (int j = 0; j < oddCount - 1; j++) {
+            if (oddNumbers[j] > oddNumbers[j + 1]) {
+                int temp = oddNumbers[j];
+                oddNumbers[j] = oddNumbers[j + 1];
+                oddNumbers[j + 1] = temp;
             }
         }
+    }
+
+    for (int i = 0; i < evenCount; i++) {
+        for (int j = 0; j < evenCount - 1; j++) {
+            if (evenNumbers[j] > evenNumbers[j + 1]) {
+                int temp = evenNumbers[j];
+                evenNumbers[j] = evenNumbers[j + 1];
+                evenNumbers[j + 1] = temp;
+            }
+        }
+    }
+
+    int j = 0;
+    for (int i = 0; i < oddCount; i++) {
+        data[j++] = oddNumbers[i];
+    }
+    for (int i = 0; i < evenCount; i++) {
+        data[j++] = evenNumbers[i];
     }
 }
 
@@ -90,7 +120,7 @@ int main() {
 
     //=================================================================
     //Optimize the sorting algorithm further by using modulo %.
-    printf("\nRunning on optimized version\n");
+    printf("\nRunning on optimized version (single thread)\n");
     randomizeData(data);
     startTime = omp_get_wtime();
     sort_data_mod(data);
